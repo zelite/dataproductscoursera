a29ebeed968acb64df89b56aee5a8798d39a5137
zelite <jcafernandes@gmail.com>
2014-10-19 10:07:28 +0200

    made the plot look a bit better (city names, different point color for selected cities)
    city names overlap a bit :(

--- server.R
+++ server.R
@@ -8,17 +8,27 @@
 library(shiny)
 library(ggplot2)
 library(TSP)
+library(grid)
+library(RColorBrewer)
+
 
 plotTour <- function(mapPlot, cities, start_city){
+  
   if (length
       (cities) <= 1)
     return()
   else{
+  colors <- brewer.pal(8, "Dark2")
   tsp <- TSP(distances[cities, cities])
   start_city <- which(labels(tsp) == start_city)
   path <- solve_TSP(tsp, method = "nn", control=list(start=start_city))
-  print(mapPlot+geom_point(data=cityList, aes(group=NULL))+
-          geom_path(data=cityList[names(path), ], aes(group=NULL)))
+  print(mapPlot+geom_point(data=cityList, aes(group=NULL), size=2, color=colors[1])+
+          #geom_path(data=cityList[names(path), ], aes(group=NULL), size=1.5, arrow=arrow()))
+          geom_point(data=cityList[names(path), ], aes(group=NULL), size=4, color=colors[3])+
+          geom_segment(data=cityList[names(path), ], aes(xend=c(tail(long, n=-1), NA), yend=c(tail(lat, n=-1), NA), group=NULL), 
+                       arrow=arrow(), size=1, colour=colors[2])+
+          geom_text(data=cityList[names(path), ], aes(label=CityName, group=NULL), hjust=0.5, vjust=1), size=0.5)
+  
 }}
 
 p <- ggplot(t, aes(x=long, y=lat, group=group))+geom_path()+theme_few()+coord_quickmap()+guides(fill=FALSE)#+theme(aspect.ratio=1)
@@ -31,7 +41,7 @@ shinyServer(function(input, output) {
   
   output$plot <- renderPlot(if (is.null(input$cities) | is.null(input$start))
                                 return()
-                            else plotTour(p, input$cities, input$start))
+                            else suppressWarnings(plotTour(p, input$cities, input$start)))
 
   
   
